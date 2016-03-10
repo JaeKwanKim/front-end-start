dab41f5fd395940b91c7745b8f9e7861ddbb65ad
JaeKwanKim <jk79thor@icloud.com>
2016-03-10 18:08:38 +0900

    Mice-IT 2교시 실습

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,26 @@
 var str="Hello World!!";
 console.log(str);
+
+var man = {
+    name: 'gi',
+    age: 30,
+    sex: 'male'
+};
+
+for(var count=0; count < 5; count++) {
+    console.log(count);
+}
+
+var date = new Date();
+
+console.log(date);
+
+//alert("안녕하세요");
+
+var who = prompt("who are you?");
+console.log(who);
+//if(man.name === 'gi') {
+//    console.log("gi")
+//} else {
+//    console.log("who?")
+//}
