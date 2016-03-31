223878579bb30524c3475112317382ee79b8f068
JaeKwanKim <jk79thor@icloud.com>
2016-03-31 16:49:15 +0900

    5주차. 구구단 템플릿 final

--- 05/app.js
+++ 05/app.js
@@ -1,5 +1,17 @@
+var str = '';
+var wrap = document.getElementById('wrap');
+var models = [];
+var template = document.getElementById('template').innerHTML;
 for(var i=1; i <= 9; i++) {
+    // str += ("<h2>"+i+"단 시작 .. </h2>");
+    models[i] = [];
+
     for(var j=1; j<=9; j++){
-        console.log(i + "*" + j + "= " + (i*j));
+        // str += (i + "*" + j + "= " + (i*j) + "<br>");
+        models[i][j] = i * j;
     }
 }
+
+var html = tmpl(template, {list: models});
+// console.log(models);
+wrap.innerHTML = html;
