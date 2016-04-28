76fddef7c4399a4a6ad289d4b97f5e9c0609b1c0
JaeKwanKim <jk79thor@icloud.com>
2016-04-28 17:38:25 +0900

    4/28 실습예제 Elevator

--- 08/app.js
+++ 08/app.js
@@ -1,4 +1,21 @@
-$('div').on('click', function(event){
-    console.log(this,event);
-});
 
+var $body = $(document.body);
+
+var $top = $('<div>')
+    .addClass('ani')
+    .hide()
+    .click(function () {
+        $body.animate({scrollTop: 0});
+    })
+    .appendTo($body);
+
+$(window).scroll(function () {
+
+    var y = $(this).scrollTop();
+    if (y >= 100) {
+        $top.fadeIn();
+    }
+    else {
+        $top.fadeOut();
+    }
+});
